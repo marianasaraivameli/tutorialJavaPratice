bb9b57a816724357ee589844f3822ddfd5a6a84b
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-25 09:25:54 -0300

    :rocket: feat: create model Status(enum)

---
+++ src/main/java/com/example/tutorialjavaexercises/model/Tutorial.java
@@ -0,0 +1,27 @@
+package com.example.tutorialjavaexercises.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 100)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
