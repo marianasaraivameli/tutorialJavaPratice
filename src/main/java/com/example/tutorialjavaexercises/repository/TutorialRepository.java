02b2914b5041010ec3779c1d6f25f8d8f63b37bb
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-25 11:26:40 -0300

    :rocket: feat: create method  findByTitleContaining with query creation

--- src/main/java/com/example/tutorialjavaexercises/repository/TutorialRepository.java
+++ src/main/java/com/example/tutorialjavaexercises/repository/TutorialRepository.java
@@ -4,6 +4,10 @@ import com.example.tutorialjavaexercises.model.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository // não precisa colocar quando usamos o Jpa Repesitory
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+
+    List<Tutorial> findByTitleContaining(String title);
 }
