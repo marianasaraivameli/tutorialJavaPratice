6d4df5f2465d8abc0136bb78d2d515cdebc40cb5
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-25 09:25:20 -0300

    :building_construction: build: initial setup

---
+++ src/main/java/com/example/tutorialjavaexercises/TutorialJavaExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialjavaexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialJavaExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialJavaExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tutorialjavaexercises/TutorialJavaExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorialjavaexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialJavaExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
