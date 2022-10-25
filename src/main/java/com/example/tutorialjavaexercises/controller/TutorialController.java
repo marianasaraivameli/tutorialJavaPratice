dedb860408bd68d5b9518c8b3424b0c97c909fad
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-25 10:51:06 -0300

    :rocket: feat: create method updateStatusPublished in TutorialController

--- src/main/java/com/example/tutorialjavaexercises/controller/TutorialController.java
+++ src/main/java/com/example/tutorialjavaexercises/controller/TutorialController.java
@@ -1,14 +1,14 @@
 package com.example.tutorialjavaexercises.controller;
 
+import com.example.tutorialjavaexercises.model.Status;
 import com.example.tutorialjavaexercises.model.Tutorial;
 import com.example.tutorialjavaexercises.service.ITutorialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tutorial")
@@ -17,9 +17,23 @@ public class TutorialController {
     @Autowired
     private ITutorialService tutorialService;
 
+
+    // TODO: tratar exception de title null
     @PostMapping
     public ResponseEntity<Tutorial> insertTutorial(@RequestBody Tutorial tutorial) {
        Tutorial newTutorial = tutorialService.insertTutorial(tutorial);
        return new ResponseEntity<>(newTutorial, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}/published")
+    public ResponseEntity<Tutorial> updateStatusPublished(@PathVariable Long id) {
+        Optional<Tutorial> optionalTutorial = tutorialService.getById(id);
+        if (optionalTutorial.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Tutorial tutorial = optionalTutorial.get();
+        tutorial.setStatus(Status.PUBLISHED);
+        tutorial = tutorialService.update(tutorial);
+        return new ResponseEntity<>(tutorial, HttpStatus.OK);
     }
 }
