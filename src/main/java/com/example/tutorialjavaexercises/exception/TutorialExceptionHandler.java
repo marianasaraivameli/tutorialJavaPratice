17427a4e18b9c95bb504bd2720b4411047e5df51
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-26 10:00:23 -0300

    :rocket: feat: implemented handleMethodArgumentNotValid on TutorialExceptionHandler

--- src/main/java/com/example/tutorialjavaexercises/exception/TutorialExceptionHandler.java
+++ src/main/java/com/example/tutorialjavaexercises/exception/TutorialExceptionHandler.java
@@ -1,16 +1,23 @@
 package com.example.tutorialjavaexercises.exception;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.InvalidPropertiesFormatException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class TutorialExceptionHandler {
+public class TutorialExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(InvalidParamException.class)
     public ResponseEntity<ExceptionDetails> handlerInvalidParamException(InvalidParamException ex) {
         return new ResponseEntity<>(
@@ -37,4 +44,22 @@ public class TutorialExceptionHandler {
 
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        List<FieldError> errorFields = ex.getFieldErrors();
+        return new ResponseEntity<>(
+                ExceptionDetails.builder()
+                        .title("Parâmetros inválidos")
+                        .message("Um ou mais campos com valor inválido")
+                        .fields(errorFields.stream().map(FieldError::getField).collect(Collectors.joining(";")))
+                        .fieldsMessages(errorFields.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(";")))
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                status
+        );
+    }
 }
