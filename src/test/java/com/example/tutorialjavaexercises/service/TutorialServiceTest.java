e545d3085936f67d2aa752fec2865c2e101a5bf6
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-28 17:57:16 -0300

    :test_tube: feat: implemented test for method getAll in TutorialServiceTest

--- src/test/java/com/example/tutorialjavaexercises/service/TutorialServiceTest.java
+++ src/test/java/com/example/tutorialjavaexercises/service/TutorialServiceTest.java
@@ -3,6 +3,7 @@ package com.example.tutorialjavaexercises.service;
 import com.example.tutorialjavaexercises.model.Status;
 import com.example.tutorialjavaexercises.model.Tutorial;
 import com.example.tutorialjavaexercises.repository.TutorialRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -10,6 +11,9 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,13 +27,18 @@ class TutorialServiceTest {
     @Mock
     private TutorialRepository repository;
 
+    @BeforeEach
+    public void setup() {
+        repository.save(new Tutorial(1L, "Title 1", "Example description", Status.DRAFT));
+    }
+
     @Test
     void insertTutorial_returnNewTutorial_whenTutorialValid() {
         BDDMockito.when(repository.save(ArgumentMatchers.any(Tutorial.class)))
-                .thenReturn(new Tutorial(1L, "Title 1", "Example description", Status.DRAFT));
+                .thenReturn(new Tutorial(2L, "Title 2", "Example description", Status.DRAFT));
 
 
-        Tutorial newTutorial = service.insertTutorial(new Tutorial(null, "Title 1", "Example description", null));
+        Tutorial newTutorial = service.insertTutorial(new Tutorial(null, "Title 2", "Example description", null));
 
         assertThat(newTutorial).isNotNull();
         assertThat(newTutorial.getId()).isPositive();
@@ -37,7 +46,18 @@ class TutorialServiceTest {
     }
 
     @Test
-    void getAll() {
+    public void getAll_returnListTutorial_whenListExist() {
+        List<Tutorial> mockListTutorial = new ArrayList<>();
+        mockListTutorial.add(new Tutorial(1L, "Title 1", "Example description", Status.DRAFT));
+
+        BDDMockito.when(repository.findAll())
+                .thenReturn(mockListTutorial);
+
+        List<Tutorial> tutorialList = service.getAll();
+
+        assertThat(tutorialList).isNotNull();
+        assertThat(tutorialList.get(0).getTitle()).isEqualTo(mockListTutorial.get(0).getTitle());
+        assertThat(tutorialList.get(0).getStatus()).isEqualTo(Status.DRAFT);
     }
 
     @Test
